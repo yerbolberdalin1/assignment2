74ef652f29f9a6250d6883454b0014d75ccf84f1
yerbolberdalin1 <jumanji2678@gmail.com>
2025-12-23 08:08:48 +0500

    1

---
+++ src/Class_restangle.java
@@ -0,0 +1,58 @@
+public class Class_restangle {
+
+    private double width;
+    private double height;
+    int id;
+    static int idGen;
+
+    public Class_restangle(){
+        this.width = 1.0;
+        this.height = 1.0;
+        this.id = idGen++;
+    }
+
+    public Class_restangle(double width, double height){
+        this();
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setWidth(double height){
+        if(width <= 0) {
+            throw new IllegalArgumentException("Height must be greater than zero!");
+        }
+        this.height=height;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double height){
+        if(height <= 0){
+            throw new IllegalArgumentException("Height must be greater than zero!");
+        }
+        this.height=height;
+    }
+
+    public double area(){
+        return height * width;
+    }
+
+    public double perimeter(){
+        return 2*(height + width);
+    }
+
+    public String toString(){
+        return "Class_restangele{" +
+                "id=" +id +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+}
